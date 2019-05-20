9c03e1b230805b2af73bd466f1b70027633dbf53
elijah johnston <eli@millinno.com>
2019-05-20 11:05:19 +0800

    restaurants and pages controller init

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'pages/about'
+  root to: 'pages#home'
+  get 'restaurants', to: 'restaurants#index'
+  post 'restaurants', to: 'restaurants#create'
+
+  # route for showing a single restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
